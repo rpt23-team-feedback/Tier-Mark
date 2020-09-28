1ca34ac0f03d3f0ee90472d9ed7c37e4d0791c9c
Mark <mark.scrudder@gmail.com>
2020-09-28 11:57:37 -0700

    Corrected small syntax errors in server index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const db = require('../database/index.js')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(path.join(__dirname, '..public'));
+app.use(express.static(path.join(__dirname, '..public')));
 
 const PORT = 3101;
 
@@ -16,11 +16,11 @@ app.listen(PORT, () => console.log('listening on port', port));
 app.get('./tiersIncluded', (req, res) => {
   const dataString = req.body;
   const tiersData = db.tiersRequest(dataString);
-  const res.send(tiersData);
+  res.send(tiersData);
 });
 
 app.get('./itemsIncluded', (req, res) => {
   const dataString = req.body;
   const itemsData = db.itemsRequest(dataString);
-  const res.send(itemsData);
+  res.send(itemsData);
 });
