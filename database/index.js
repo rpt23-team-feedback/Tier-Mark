93dcd2d021b29fbf507b5ff5d08b1cb3fce3d34a
Mark <mark.scrudder@gmail.com>
2020-10-20 15:51:08 -0700

    Refactored seed script.

--- database/index.js
+++ database/index.js
@@ -3,7 +3,6 @@ const mysqlConfig = require('./config.js');
 const { Sequelize } = require('sequelize');
 const { user } = require('./config.js');
 const sequelize = new Sequelize('humblebundle', 'root', '', { host: 'localhost', dialect: 'mysql'});
-const Seed = require('./seed.js'); // Not used in this build, will be reimplemented
 
 const connection = mysql.createConnection(mysqlConfig);
 
@@ -20,43 +19,6 @@ var Tiers = sequelize.define('tiers', {
   itemOrder: Sequelize.INTEGER
 });
 
-const seed = () => {
-  Bundles.drop()
-  .then(() => {
-    Bundles.sync({ force: true })
-    .then(() => {
-      data = Seed.seedBundles();
-      Bundles.bulkCreate(data, { validate: true })
-      .then(() => {
-        console.log('bundles created');
-      })
-      .catch(err => {
-        console.log('failed to create bundles', err);
-      })
-      .finally(() => {
-        sequelize.close();
-      })
-    })
-  });
-
-  Tiers.drop()
-  .then(() => {
-    Tiers.sync({ force: true })
-    .then(() => {
-      data = Seed.seedTiers();
-      Tiers.bulkCreate(data, { validate: true})
-      .then(() => {
-        console.log('tiers created');
-      })
-      .catch(err => {
-        console.log('failed to create tiers', err);
-      })
-      .finally(() => {
-        sequelize.close();
-      })
-    })
-  });
-};
 
 const tiersRequest = (data) => {
   let bundleId = data.bundleId;
@@ -75,7 +37,6 @@ const tiersRequest = (data) => {
 
 const itemsRequest = (data) => {
   let tierId = data.tierId;
-  // let tierId = '1';
   return Tiers.findAll({
     where: {
       tierId: tierId
@@ -90,5 +51,5 @@ const itemsRequest = (data) => {
 };
 
 module.exports = {
-  Bundles, Tiers, seed, tiersRequest, itemsRequest
+  Bundles, Tiers, sequelize, tiersRequest, itemsRequest
 };
--- database/seed.js
+++ database/seed.js
@@ -4,27 +4,27 @@ const db = require('./index.js');
 // for each bundle 1-100
 // 3 random tiers
 // random cost 5-20
-const seedBundles = () => {
+function seedBundles() {
   var bundleId, tierId1, tierId2, tierId3, cost;
   var bundleData = [];
   for (let i = 0; i <= 99; i++) {
     bundleId = i + 1;
     cost = Math.floor(Math.random() * 4) * 5;
-    tierId1 = i*3 + 1;
-    tierId2 = i*3 + 2;
-    tierId3 = i*3 + 3;
+    tierId1 = i * 3 + 1;
+    tierId2 = i * 3 + 2;
+    tierId3 = i * 3 + 3;
     bundleData.push({
-      bundleId : bundleId,
-      tierId : tierId1,
+      bundleId: bundleId,
+      tierId: tierId1,
       tierOrder: 1
     }, {
-      bundleId : bundleId,
-      tierId : tierId2,
-      cost : cost,
+      bundleId: bundleId,
+      tierId: tierId2,
+      cost: cost,
       tierOrder: 2
     }, {
-      bundleId : bundleId,
-      tierId : tierId3,
+      bundleId: bundleId,
+      tierId: tierId3,
       tierOrder: 3
     });
   }
@@ -35,61 +35,93 @@ const seedBundles = () => {
 // 2 - 6 items for each
 // each item should come from different segment
 
-const seedTiers = () => {
+function seedTiers() {
   var tierId, itemId;
   var tierData = [];
   for (let i = 1; i <= 300; i++) {
     tierId = i;
     tierOrder = 1;
     itemId = Math.floor(Math.random() * 48) + 1;
-    tierData.push ({
-      tierId : tierId,
-      itemId : itemId,
+    tierData.push({
+      tierId: tierId,
+      itemId: itemId,
       itemOrder: tierOrder += 1
     });
     itemId = Math.floor(Math.random() * 48) + 49;
-    tierData.push ({
-      tierId : tierId,
-      itemId : itemId,
+    tierData.push({
+      tierId: tierId,
+      itemId: itemId,
       itemOrder: tierOrder += 1
     });
-    if (Math.random() < .3 ) {
+    if (Math.random() < .3) {
       itemId = Math.floor(Math.random() * 48) + 97;
-      tierData.push ({
-        tierId : tierId,
-        itemId : itemId,
+      tierData.push({
+        tierId: tierId,
+        itemId: itemId,
         itemOrder: tierOrder += 1
       });
     }
-    if (Math.random() < .3 ) {
+    if (Math.random() < .3) {
       itemId = Math.floor(Math.random() * 48) + 145;
-      tierData.push ({
-        tierId : tierId,
-        itemId : itemId,
+      tierData.push({
+        tierId: tierId,
+        itemId: itemId,
         itemOrder: tierOrder += 1
       });
     }
-    if (Math.random() < .3 ) {
+    if (Math.random() < .3) {
       itemId = Math.floor(Math.random() * 48) + 193;
-      tierData.push ({
-        tierId : tierId,
-        itemId : itemId,
+      tierData.push({
+        tierId: tierId,
+        itemId: itemId,
         itemOrder: tierOrder += 1
       });
     }
-    if (Math.random() < .3 ) {
+    if (Math.random() < .3) {
       itemId = Math.floor(Math.random() * 48) + 241;
-      tierData.push ({
-        tierId : tierId,
-        itemId : itemId,
+      tierData.push({
+        tierId: tierId,
+        itemId: itemId,
         itemOrder: tierOrder += 1
-      })
+      });
     }
   }
   return tierData;
 }
 
+const seed = () => {
+  db.Bundles.drop()
+  .then(() => {
+    db.Bundles.sync({ force: true })
+    .then(() => {
+      let data = seedBundles();
+      db.Bundles.bulkCreate(data, { validate: true })
+      .then(() => {
+        console.log('bundles created');
+      })
+      .catch(err => {
+        console.log('failed to create bundles', err);
+      })
+    })
+  });
 
+  db.Tiers.drop()
+  .then(() => {
+    db.Tiers.sync({ force: true })
+    .then(() => {
+      let data = seedTiers();
+      db.Tiers.bulkCreate(data, { validate: true})
+      .then(() => {
+        console.log('tiers created');
+      })
+      .catch(err => {
+        console.log('failed to create tiers', err);
+      })
+    })
+  });
+};
+
+seed();
 
 module.exports = {
   seedBundles, seedTiers
