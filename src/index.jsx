882cbd99e436b840f25e744b58bb96844e5ff44e
Mark <mark.scrudder@gmail.com>
2020-10-30 15:14:00 -0700

    Refactored for React Router.

--- src/index.jsx
+++ src/index.jsx
@@ -1,63 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Tier from './components/tier.jsx';
-import $ from 'jquery';
-import './index.css';
-
-const awsAddress = 'http://3.15.215.14:3101/';
+import { BrowserRouter, Route, Redirect, Switch, useParams } from 'react-router-dom';
+import Bundle from './components/bundle.jsx';
 
 class Tiers extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      bundleId: 1,
-      tier1Id: 1,
-      tier1Cost: 1,
-      tier2Id: 2,
-      tier2Cost: 3, // from Top Contributors
-      tier3Id: 3,
-      tier3Cost: 25,
-    }
-    this.getBundleData = this.getBundleData.bind(this);
-  }
-
-  componentDidMount() {
-    this.getBundleData();
-  }
-
-  getBundleData() {
-    var url = new URL(window.location);
-    var bundleId = url.searchParams.get('bundle');
-
-    $.ajax({
-      method: 'GET',
-      url: `${awsAddress}tiersIncluded/${bundleId}`,
-      datatype: 'json',
-      success: (data) => {
-        this.setState({
-          bundleId: bundleId,
-          tier1Id: data.tier1Id,
-          tier2Id: data.tier2Id,
-          tier3Id: data.tier3Id,
-          tier3Cost: data.tier3Cost,
-        });
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    })
   }
 
   render() {
     return (
-      <div className="tiers-container">
-        Bundle {this.state.bundleId}
-        <Tier key={this.state.tier1Id} tierId={this.state.tier1Id} tierCost={this.state.tier1Cost} tierType="1"/>
-        <Tier key={this.state.tier2Id} tierId={this.state.tier2Id} tierCost={this.state.tier2Cost} tierType="2"/>
-        <Tier key={this.state.tier3Id} tierId={this.state.tier3Id} tierCost={this.state.tier3Cost} tierType="3"/>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Redirect exact from='/' to='/1' />
+          <Route path='/:bundleId' component={Bundle} />
+        </Switch>
+      </BrowserRouter>
     )
   }
+
 };
 
 ReactDOM.render(<Tiers />, document.getElementById('Tiers'));
